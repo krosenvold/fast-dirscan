292b6855254500b3fd7b9da240721ca34a01baa3
Kristian Rosenvold <krosenvold@apache.org>
2013-11-14 21:17:20 +0100

    Changed to fork/join pool

--- src/main/java/org/smartscan/MultiReader.java
+++ src/main/java/org/smartscan/MultiReader.java
@@ -7,8 +7,11 @@ import org.smartscan.tools.ScannerTools;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -23,7 +26,7 @@ public class MultiReader
 
     private final AtomicInteger threadsStarted = new AtomicInteger( 1 );
 
-    private final ExecutorService executor;
+    private final ForkJoinPool executor;
 
     private final FastFileReceiver fastFileReceiver;
 
@@ -41,7 +44,7 @@ public class MultiReader
     {
         super( basedir, includes, excludes );
         ScannerTools.verifyBaseDir( basedir );
-        executor = Executors.newFixedThreadPool( nThreads );
+        executor = new ForkJoinPool( nThreads );
         this.fastFileReceiver = fastFileReceiver;
     }
 
@@ -136,9 +139,9 @@ public class MultiReader
                             else
                             {
 
-                                final Runnable target = new AsynchScanner( file, copy( mutablevpath ) );
+                                final AsynchScanner target = new AsynchScanner( file, copy( mutablevpath ) );
                                 threadsStarted.incrementAndGet();
-                                executor.submit( target );
+                                executor.submit( ForkJoinTask.adapt( target ));
                             }
                         }
                     }
@@ -151,8 +154,7 @@ public class MultiReader
     }
 
 
-    class AsynchScanner
-        implements Runnable
+    class AsynchScanner implements Callable
     {
         File dir;
 
@@ -165,16 +167,19 @@ public class MultiReader
         }
 
         @Override
-        public void run()
+        public Object call()
+            throws Exception
         {
             try
             {
 
                 asynchscandir( dir, vpath );
+                return true;
             }
             catch ( Throwable e )
             {
                 e.printStackTrace();
+                return false;
             }
         }
     }
