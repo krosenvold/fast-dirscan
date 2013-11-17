3e9e33bd2b6340892b062552d4a735b7849fea52
Kristian Rosenvold <krosenvold@apache.org>
2013-11-17 19:12:36 +0100

    Pushed

--- src/test/java/org/smartscan/BenchmarkTest.java
+++ src/test/java/org/smartscan/BenchmarkTest.java
@@ -51,6 +51,7 @@ public class BenchmarkTest
             assertThat( multiThreadedSingleReceiver( file, 8 ) ).as( "8 mtsr" ).isEqualTo( expected );
             assertThat( multiThreadedSingleReceiver( file, 4 ) ).as( "4 mtsr" ).isEqualTo( expected );
             assertThat( multiThreaded( file, 12 ) ).as( "mr" ).isEqualTo( expected );
+			assertThat( multiThreaded( file, 16 ) ).as( "mr" ).isEqualTo( expected );
 
             System.out.println( "" );
         }
