2281e6f7a5709a1cb07ab3b62be58718b7e4ddf0
Kristian Rosenvold <krosenvold@apache.org>
2013-11-17 19:27:57 +0100

    Less threades

--- src/test/java/org/smartscan/BenchmarkTest.java
+++ src/test/java/org/smartscan/BenchmarkTest.java
@@ -49,10 +49,9 @@ public class BenchmarkTest
             assertThat( multiThreadedSingleReceiver(file, 12) ).describedAs("12 mtsr").isEqualTo( expected  );
             assertThat( multiThreadedSingleReceiver( file, 8 ) ).as("8 mtsr").isEqualTo( expected );
             assertThat( multiThreadedSingleReceiver( file, 4 ) ).as( "4 mtsr" ).isEqualTo( expected );
+			assertThat( multiThreaded( file, 10 ) ).as( "mr" ).isEqualTo( expected );
             assertThat( multiThreaded( file, 12 ) ).as( "mr" ).isEqualTo( expected );
 			assertThat( multiThreaded(file, 16) ).as( "mr" ).isEqualTo( expected );
-			assertThat( multiThreaded( file, 20 ) ).as( "mr" ).isEqualTo( expected );
-
             System.out.println( "" );
         }
 
