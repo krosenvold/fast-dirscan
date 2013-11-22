0d2829b0d084fe34cf5ccf0f8a684dfeadced3dd
Kristian Rosenvold <krosenvold@apache.org>
2013-11-22 19:17:32 +0100

    Pimped

--- src/main/java/org/smartscan/tools/CachingMultiReader.java
+++ src/main/java/org/smartscan/tools/CachingMultiReader.java
@@ -17,120 +17,101 @@ import java.util.concurrent.RecursiveAction;
  * Reads with multiple threads
  */
 public class CachingMultiReader
-    extends ModernBase
-{
+		extends ModernBase {
 
-    public static final char[][] NO_FILES_VPATH_ = new char[0][];
+	public static final char[][] NO_FILES_VPATH_ = new char[0][];
 
-    private final ForkJoinPool executor;
+	private final ForkJoinPool executor;
 
-    private final SmartFileReceiver smartFileReceiver;
+	private final SmartFileReceiver smartFileReceiver;
 
 	private final ScanCache scanCache;
 
 	public CachingMultiReader(File basedir, Filters includes, Filters excludes, SmartFileReceiver smartFileReceiver,
-							  int nThreads)
-	{
-		super( basedir, includes, excludes );
-		ScannerTools.verifyBaseDir( basedir );
-		executor = new ForkJoinPool( nThreads );
+							  int nThreads) {
+		super(basedir, includes, excludes);
+		ScannerTools.verifyBaseDir(basedir);
+		executor = new ForkJoinPool(nThreads);
 		this.smartFileReceiver = smartFileReceiver;
 		scanCache = new ScanCache();
 	}
 
-	public boolean isComplete(){
+	public boolean isComplete() {
 		final boolean quiescent = executor.isQuiescent();
-		if (quiescent){
+		if (quiescent) {
 			executor.shutdown();
 		}
 		return quiescent;
 	}
 
-    @SuppressWarnings("StatementWithEmptyBody")
-	public void awaitCompletion()
-    {
-		while (!executor.isQuiescent()) {doSleep(1);}
-        executor.shutdown();
-    }
+	@SuppressWarnings("StatementWithEmptyBody")
+	public void awaitCompletion() {
+		while (!executor.isQuiescent()) {
+			doSleep(1);
+		}
+		executor.shutdown();
+	}
 
 	public void beginThreadedScan()
-        throws IllegalStateException, InterruptedException
-    {
-        Runnable scanner = new Runnable()
-        {
-            @Override
-            public void run()
-            {
+			throws IllegalStateException, InterruptedException {
+		Runnable scanner = new Runnable() {
+			@Override
+			public void run() {
 				scandir(Java7SmartFile.createSmartFile(basedir, NO_FILES_VPATH_), NO_FILES_VPATH_);
 			}
-        };
-        executor.submit(scanner);
-    }
+		};
+		executor.submit(scanner);
+	}
 
 
-	@SuppressWarnings( "AssignmentToMethodParameter" )
-    private void scandir( SmartFile parent, char[][] unmodifyableparentvpath )
-    {
-        @Nullable SmartFile firstDir;
-        @Nullable char[][] firstVpath;
-        do
-        {
-            firstDir = null;
-            firstVpath = null;
+	@SuppressWarnings("AssignmentToMethodParameter")
+	private void scandir(SmartFile parent, char[][] unmodifyableparentvpath) {
+		@Nullable SmartFile firstDir;
+		@Nullable char[][] firstVpath;
+		do {
+			firstDir = null; firstVpath = null;
+			File[] newfiles = parent.listFiles();
+			if (newfiles == null) return;
+			char[][] mutablevpath = copyWithOneExtra(unmodifyableparentvpath);
+			final int vpathIdx = mutablevpath.length - 1;
+			for (final SmartFile smartFile : createSmartFiles(newfiles, unmodifyableparentvpath)) {
+				mutablevpath[vpathIdx] = smartFile.getFileNameChar();
 
-            File[] newfiles = parent.listFiles();
+				boolean shouldInclude = shouldInclude(mutablevpath);
 
-            if ( newfiles != null )
-            {
-				char[][] mutablevpath = copyWithOneExtra(unmodifyableparentvpath);
-				for (final SmartFile smartFile : createSmartFiles(newfiles, unmodifyableparentvpath)) {
-					mutablevpath[mutablevpath.length - 1] = smartFile.getFileNameChar();
+				if (smartFile.isFile()) {
+					if (shouldInclude) {
+						smartFileReceiver.accept(smartFile);
+					}
+				} else if (smartFile.isDirectory()) {
+					if (shouldInclude || couldHoldIncluded(mutablevpath)) {
+						if (firstDir == null) {
+							firstDir = smartFile;
+							firstVpath = copy(mutablevpath);
+						} else {
+							final char[][] copy = copy(mutablevpath);
+							new RecursiveAction() {
+								@Override
+								protected void compute() {
+									scandir(smartFile, copy);
+								}
+							}.fork(); // Todo: fix swallowed exceptions
+						}
+					}
+				}
+			}
+			parent = firstDir;
+			unmodifyableparentvpath = firstVpath;
+		}
+		while (firstDir != null);
+	}
 
-                    boolean shouldInclude = shouldInclude( mutablevpath );
-                    if ( smartFile.isFile() )
-                    {
-                        if ( shouldInclude )
-                        {
-                            smartFileReceiver.accept(smartFile);
-                        }
-                    }
-                    else if ( smartFile.isDirectory() )
-                    {
-                        if ( shouldInclude || couldHoldIncluded( mutablevpath ) )
-                        {
-                            if ( firstDir == null )
-                            {
-                                firstDir = smartFile;
-                                firstVpath = copy( mutablevpath );
-                            }
-                            else
-                            {
-								final char[][] copy = copy(mutablevpath);
-								new RecursiveAction() {
-									@Override
-									protected void compute() {
-										scandir(smartFile, copy);
-									}
-								}.fork();
-								// Todo: fix swallowed exceptions
-                            }
-                        }
-                    }
-                }
-            }
-            parent = firstDir;
-            unmodifyableparentvpath = firstVpath;
-        }
-        while ( firstDir != null );
-    }
-
-	private static SmartFile[] createSmartFiles(File[] files, char[][] unmodifyableparentvpath){
+	private static SmartFile[] createSmartFiles(File[] files, char[][] unmodifyableparentvpath) {
 
 		final int length = files.length;
 		SmartFile[] result = new SmartFile[length];
 
-		for (int i = 0; i < length; i++)
-		{
+		for (int i = 0; i < length; i++) {
 			result[i] = Java7SmartFile.createSmartFile(files[i], unmodifyableparentvpath);
 		}
 		return result;
